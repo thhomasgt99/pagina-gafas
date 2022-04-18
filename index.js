057aa0951d91fb820767b21e66fb340bf5235284
Thomas Bernal <thhomasgt99@gmail.com>
2022-04-18 16:49:36 -0500

    Se añadio el archivo md, arreglaron estilos y se cambio el logo de pagina

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 const slider = document.querySelector("#slider")
 let sliderSection = document.querySelectorAll(".slider__section")
-let sliderSectionLast = sliderSection[sliderSection.length -1]
+let sliderSectionLast = sliderSection[sliderSection.length - 1]
 
 const bntLeft = document.querySelector("#btn-left")
 const bntRight = document.querySelector("#btn-right")
 
 slider.insertAdjacentElement('afterbegin', sliderSectionLast)
 
-function Next(){
+function Next() {
    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0]
    slider.style.marginLeft = "-200%"
    slider.style.transition = "all 1s"
-    setTimeout(function(){
-       slider.style.transition = "none"
-       slider.insertAdjacentElement('beforeend',sliderSectionFirst)
-       slider.style.marginLeft ="-100%"
-   },1000)
+   setTimeout(function () {
+      slider.style.transition = "none"
+      slider.insertAdjacentElement('beforeend', sliderSectionFirst)
+      slider.style.marginLeft = "-100%"
+   }, 1000)
 }
 
-function Prev(){
+function Prev() {
    let sliderSection = document.querySelectorAll(".slider__section")
-   let sliderSectionLast = sliderSection[sliderSection.length -1]
+   let sliderSectionLast = sliderSection[sliderSection.length - 1]
    slider.style.marginLeft = "0"
    slider.style.transition = "all 1s"
-  setTimeout(function(){
-       slider.style.transition = "none"
-       slider.insertAdjacentElement('afterbegin', sliderSectionLast)
-       slider.style.marginLeft ="-100%"
-   },1000)
+   setTimeout(function () {
+      slider.style.transition = "none"
+      slider.insertAdjacentElement('afterbegin', sliderSectionLast)
+      slider.style.marginLeft = "-100%"
+   }, 1000)
 }
 
-bntRight.addEventListener('click', function(){
+bntRight.addEventListener('click', function () {
    Next()
 })
 
-bntLeft.addEventListener('click', function(){
+bntLeft.addEventListener('click', function () {
    Prev()
 })
 
 setInterval(() => {
-  Next() 
+   Next()
 }, 5000);
 
